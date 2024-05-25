b18a39549307a1512dbb26f7bec0d489ae0c4aa2
UTHRATHITHAN <m.uthrathithan@gmail.com>
2024-05-25 23:59:31 +0530

    Env variable added

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-env:{
-    NEXT_PUBLIC_BACKEND_URL :"http://localhost:8000"
-}
+    env: {
+        NEXT_PUBLIC_BACKEND_URL: "https://presidio-assignment-a2d6.vercel.app"
+    }
 };
 
 export default nextConfig;
