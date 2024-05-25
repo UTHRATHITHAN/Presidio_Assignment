d73230fcb8c5752de205ade41009943a445ece29
UTHRATHITHAN <m.uthrathithan@gmail.com>
2024-05-26 01:01:00 +0530

    Next Js config updated

--- backend/src/index.js
+++ backend/src/index.js
@@ -27,7 +27,11 @@ app.use(express.json())
 app.use(cors({ origin: process.env.ORIGIN, credentials: true }))
 app.use(urlencoded({ extended: true }))
 app.use(cookieParser())
-
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+  });
 app.post("/register", async (req, res) => {
     const formData = req.body;
     console.log(formData);
