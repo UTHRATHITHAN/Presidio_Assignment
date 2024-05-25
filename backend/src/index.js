7cc318b4e158326d41b5969ae8a8879078303191
UTHRATHITHAN <m.uthrathithan@gmail.com>
2024-05-25 23:27:20 +0530

    Code Refractored

--- backend/src/index.js
+++ backend/src/index.js
@@ -24,7 +24,7 @@ const prisma = new PrismaClient()
 
 const app = express();
 app.use(express.json())
-app.use(cors({ origin: "http://localhost:3000", credentials: true }))
+app.use(cors({ origin: process.env.ORIGIN, credentials: true }))
 app.use(urlencoded({ extended: true }))
 app.use(cookieParser())
 
